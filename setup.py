8d730addc4bbbcfbb691cd8cf5170c31c50805d3
Eric Urban <hydrogen18@gmail.com>
2020-02-09 21:39:24 -0600

    Add packaging stuff

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+DESC = 'A python package to wait for multiple awaitables'
+
+
+setuptools.setup(
+name='multi-await',
+version='1.0.0',
+author='Eric Urban',
+author_email='hydrogen18@gmail.com',
+description=DESC,
+long_description=DESC,
+url='https://github.com/hydrogen18/multi-await',
+packages=setuptools.find_packages(),
+classifiers=[
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: OS Independent",
+],
+python_requires='>=3.7',
+)
